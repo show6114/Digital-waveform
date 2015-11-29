a928c5877d68d390e2730cfdd195f802e35ae35e
Zai-Wun Lin <show6114@gmail.com>
2015-11-29 22:03:43 +0800

    Finish canvas draw

--- js/app.js
+++ js/app.js
@@ -1,71 +1,109 @@
 (function() {
-	/**
-	* digitalWaveform Module
-	*
-	* Description
-	*/
-	var app = angular.module('digitalWaveform', []);
+    /**
+     * digitalWaveform Module
+     *
+     * Description
+     */
+    var app = angular.module('digitalWaveform', []);
 
-	app.controller('WaveformViewController', function () {
-		var wfb = new WaveformBrain([1, 0, 1, 1]);
-		
-		this.addLogic = function (logic) {
-			wfb.addDigit(logic);
-		}
-		this.clearAllLogic = function () {
-			wfb.clearAllDigit();
-		}
+    app.controller('WaveformViewController', function() {
+        var wfb = new WaveformBrain([0, 1]);
 
-		this.digits = wfb;
-	});
+        this.digits = wfb;
 
-	app.directive('waveformDraw', function(){	
-		return {
-			restrict: 'A',
-			scope: {
-				data: '='
-			}, 
-			link: function (scope, element, attrs) {
-				var ctx = element[0].getContext('2d');
-				var h = attrs.height;
-				// for (var i = 0; i < attrs.waveformDraw.length; i++) {
-				// 	console.log();
-				// }
+        this.addLogic = function(logic) {
+            wfb.addDigit(logic);
+        }
 
-	            ctx.moveTo(0, 0);
-	            ctx.lineTo(30, 0);
-	            ctx.lineTo(30, 30);
-	            ctx.lineTo(720, 30);
+        this.clearAllLogic = function() {
+            wfb.clearAllDigit();
+        }
+    });
 
-	            ctx.lineWidth=5;
-	            ctx.stroke();
-	            console.log(scope.data);
-			}
-		};
-	});
+    app.directive('waveformDraw', function() {
+        return {
+            restrict: 'A',
+            scope: {
+                waveformDraw: '='
+            },
+            link: function(scope, element, attrs) {
+                var ctx = element[0].getContext('2d');
+                var h = Number(attrs.height);
 
-	// model of digital waveform
-	function WaveformBrain (initDdata) {
-		// initialize
-		this.ddata = initDdata || [];
-		this.clock = updateClock(initDdata);
+                scope.$watchCollection('waveformDraw', function(newValue) {
+                    if (newValue.length > 0)
+                        canvasWaveformDraw(ctx, newValue, h);
+                    else
+                        clearCanvasWaveformDraw(ctx, attrs.width, h);
+                });
 
-		this.addDigit = function (logic) {
-			this.ddata.push(logic);
-			this.clock = updateClock(this.ddata);
-		};
+                function canvasWaveformDraw(ctx, data, h) {
+                    var w = h * 1.2;
 
-		this.clearAllDigit = function () {
-			this.ddata = [];
-			this.clock = [];
-		}
+                    function initial(logic) {
+                        this.x = 0;
+                        this.y = logic ? 0 : h;
+                        ctx.beginPath();
+                        ctx.moveTo(this.x, this.y);
+                    }
 
-		function updateClock (ddata) {
-			var clock = [];
-			for (var i = 0; i < ddata.length; i++) {
-				clock[i] = i % 2;
-			}
-			return clock;
-		};
-	}
+                    function getLogic() {
+                        return (this.y > h / 2) ? 0 : 1;
+                    }
+
+                    function drawLevel() {
+                        var newX = this.x += w;
+                        ctx.lineTo(newX, this.y);;
+                    }
+
+                    function drawEdge() {
+                        var edge = (this.y > h / 2) ? -1 : 1;
+                        var newY = this.y += edge * h;
+                        ctx.lineTo(this.x, newY);
+                    }
+
+
+                    initial(data[0]);
+                    drawLevel();
+                    for (var i = 1; i < data.length; i++) {
+                        if (getLogic() != data[i]) drawEdge();
+
+                        drawLevel();
+                    }
+
+                    ctx.lineWidth = 3;
+                    ctx.stroke();
+                }
+
+                function clearCanvasWaveformDraw(ctx, w, h) {
+                    ctx.clearRect(0, 0, w, h);
+                }
+            }
+        };
+    });
+
+    // model of digital waveform
+    function WaveformBrain(initDdata) {
+        // initialize
+        this.ddata = initDdata;
+        this.clock = updateClock(initDdata);
+
+        this.addDigit = function(logic) {
+            this.ddata.push(logic);
+            this.clock = updateClock(this.ddata);
+        };
+
+        this.clearAllDigit = function() {
+            this.ddata = [];
+            this.clock = [];
+        }
+
+        function updateClock(ddata) {
+            var clock = [];
+            for (var i = 0; i < ddata.length; i++) {
+                clock[i] = i % 2;
+            }
+            return clock;
+        };
+    }
 })();

4ce82f8d2818936c1f02de5e498033c541615aaf
Zai-Wun Lin <show6114@gmail.com>
2015-11-30 15:55:35 +0800

    Add offset to UI

--- js/app.js
+++ js/app.js
@@ -1,53 +1,68 @@
 (function() {
-    /**
-     * digitalWaveform Module
-     *
-     * Description
-     */
     var app = angular.module('digitalWaveform', []);
 
     app.controller('WaveformViewController', function() {
+        // initialize
         var wfb = new WaveformBrain();
-        wfb.addClkOffset(1);
-        this.waveform = wfb;
+        this.clockOffsetValue = wfb.channels.clk.offset * 100;
 
-        this.addLogic = function(logic) {
-            wfb.addDigit(logic);
+        this.addLogicHigh = function() {
+            wfb.addDigit(1);
+        }
+
+        this.addLogicLow = function() {
+            wfb.addDigit(0);
         }
 
         this.clearAllLogic = function() {
             wfb.clearAllDigit();
         }
+
+        this.updateClockOffset = function() {
+            wfb.addClkOffset(this.clockOffsetValue / 100);
+        }
+
+        this.waveform = wfb;
     });
 
     app.directive('waveformDraw', function() {
         return {
             restrict: 'A',
             scope: {
-                waveformDraw: '=', 
+                waveformData: '=',
                 waveformOffset: '='
             },
             link: function(scope, element, attrs) {
                 var ctx = element[0].getContext('2d');
-                var bitHeight = Number(attrs.height);
-                var bitWidth = Number(attrs.width);
+                var offset = scope.waveformOffset;
+                var box = {
+                    'h': Number(attrs.height),
+                    'w': Number(attrs.width)
+                };
 
-                console.log(scope.waveformOffset);
-                scope.$watchCollection('waveformDraw', function(channelBits) {
-                    if (channelBits.length > 0)
-                        canvasWaveformDraw(ctx, channelBits, bitHeight);
+                scope.$watchCollection('waveformData', function(newValue) {
+                    var dataBits = newValue;
+                    if (dataBits.length > 0)
+                        canvasWaveformDraw(ctx, dataBits, box, offset);
                     else
-                        clearCanvasWaveformDraw(ctx, bitWidth, bitHeight);
+                        clearCanvasWaveformDraw(ctx, box);
                 });
 
-                function canvasWaveformDraw(ctx, data, h) {
+                function canvasWaveformDraw(ctxObj, data, boxSize, offset) {
+                    var h = boxSize.h;
                     var w = h * 1.2;
+                    var offsetW = offset * w;
 
                     function initial(logic) {
                         this.x = 0;
                         this.y = logic ? 0 : h;
-                        ctx.beginPath();
-                        ctx.moveTo(this.x, this.y);
+                        ctxObj.beginPath();
+                        ctxObj.moveTo(this.x, this.y);
+                    }
+
+                    function drawOffset(value) {
+                        var newX = this.x += value;
+                        ctxObj.lineTo(newX, this.y);;
                     }
 
                     function getLogic() {
@@ -56,17 +71,17 @@
 
                     function drawLevel() {
                         var newX = this.x += w;
-                        ctx.lineTo(newX, this.y);;
+                        ctxObj.lineTo(newX, this.y);;
                     }
 
                     function drawEdge() {
                         var edge = (this.y > h / 2) ? -1 : 1;
                         var newY = this.y += edge * h;
-                        ctx.lineTo(this.x, newY);
+                        ctxObj.lineTo(this.x, newY);
                     }
 
-
                     initial(data[0]);
+                    drawOffset(offsetW);
                     drawLevel();
                     for (var i = 1; i < data.length; i++) {
                         if (getLogic() != data[i]) drawEdge();
@@ -78,8 +93,8 @@
                     ctx.stroke();
                 }
 
-                function clearCanvasWaveformDraw(ctx, w, h) {
-                    ctx.clearRect(0, 0, w, h);
+                function clearCanvasWaveformDraw(ctxObj, boxSize) {
+                    ctxObj.clearRect(0, 0, boxSize.w, boxSize.h);
                 }
             }
         };
@@ -90,13 +105,15 @@
         // initialize
         this.channels = {
             'dat': {
-                'bits': [1, 0], 
-                'offset': 0, 
-                'name': 'DATA'}, 
+                'bits': [1, 0],
+                'offset': 0,
+                'name': 'DATA'
+            },
             'clk': {
-                'bits': [0, 1], 
-                'offset': 0, 
-                'name': 'CLOCK'}
+                'bits': [0, 1],
+                'offset': 0.5,
+                'name': 'CLOCK'
+            }
         };
 
         this.addDigit = function(logic) {
@@ -110,6 +127,7 @@
             this.channels.dat.bits = [];
             this.channels.clk.bits = [];
         }
+
         function updateClock(dat) {
             var clk = [];
             for (var i = 0; i < dat.length; i++) {
